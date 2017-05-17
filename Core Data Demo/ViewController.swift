d55a633a23764783303e5de99217166a70eaf082
anm8tr <threeduser@hotmail.com>
2017-05-17 10:52:00 -0700

    created AppDelegate, added variables to refer to AppDelegate, context variable to reference persistentContainer, added user variables

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.share().delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newUser.setValue("rob", forKey: "username")
+        newUser.setValue("myPass", forKey: "password")
+        newUser.setValue(35, forKey: "age")
+        
+        do {
+            
+            try context.save()
+            
+            print("Saved")
+            
+        } catch {
+            
+            print("There was an error")
+        }
     }
 
     override func didReceiveMemoryWarning() {
